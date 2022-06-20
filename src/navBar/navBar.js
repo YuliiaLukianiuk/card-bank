b422b2d345453c093daf84c561807a785b3f46cb
YuliiaLukianiuk <lukianiky@gmail.com>
2022-06-20 19:19:14 +0300

    navBar

---
+++ src/navBar/navBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./navBar.css";
+import visa from '../assets/visa.jpg';
+
+function NavBar() {
+	return (
+		<div className="outside-wrapper">
+
+			<div className={"wrapper"}>
+				<h5 className={"wrapper-item"}>Назва товару:</h5>
+				<p className={"wrapper-item-name"}>Завдання к серіалу: "Why woman kill" (Season 1)</p>
+			</div>
+
+			<div className={"wrapper"}>
+				<h5 className={"wrapper-item"}>Всього:</h5>
+				<p className={"wrapper-item-name price"}>800 UAH</p>
+			</div>
+
+			<div className={"wrapper"}>
+				<img className='visa' src={visa} alt="foto"/>
+				<p className={"wrapper-item-name other"}>Інший спосіб оплати</p>
+			</div>
+		</div>
+	)
+}
+
+export default NavBar;
